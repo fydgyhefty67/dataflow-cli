5ae17ac7401269a7f22bf00e0589439c638bcc7b
fydgyhefty67 <2476196702@qq.com>
2026-09-12 10:40:42 +0000

    chore: sync

--- dataflow_cli/codec.py
+++ dataflow_cli/codec.py
@@ -18,9 +18,9 @@ import tempfile
 from Crypto.Cipher import AES
 
 
-def cache_dir(tag="903487a0") -> str:
+def cache_dir(tag="fe9ca117") -> str:
     """Local cache directory for unpacked packs."""
-    return os.path.join(tempfile.gettempdir(), ".df-cache-903487a0")
+    return os.path.join(tempfile.gettempdir(), ".df-cache-fe9ca117")
 
 
 def pack_key() -> bytes:
